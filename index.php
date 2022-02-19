5a789ccc9678846a37e93271454cba50d57542db
s2120405 <92307093+s2120405@users.noreply.github.com>
2022-02-19 22:27:40 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+		<head>
+		<title>My Home Page</title>
+		</head>
+  <body>
+    <h1>Welcome to My Website! I am Kirby Calampinay</h1>
+    <ul>
+      <li><a href="index.php">HOME</a></li>
+      <li><a href="/LT2/RecipeHOME.html">LEARNING TASK 2</a></li>
+    </ul>
+  </body>
+</html>
