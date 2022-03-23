5c6448fc4ee657490f31da83141eca02d94db101
s2120405 <92307093+s2120405@users.noreply.github.com>
2022-03-23 20:34:52 +0800

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
       <li><a href="index.php">HOME</a></li>
       <li><a href="/LT2/RecipeHOME.html">LEARNING TASK 2</a></li>
 	<li><a href="/LT3/MyList.html">LEARNING TASK 3</a></li>
-	    <li><a href="/CLE1MT/Homepage.html">Culminating Learning Evidence (Midterm)</a></li>
+	    <li><a href="/CLE1MT/index.html">Culminating Learning Evidence (Midterm)</a></li>
     </ul>
   </body>
 </html>
