57c4f79ddd6b4aa1f92245e6ab58dfebf01c7c05
s2120405 <92307093+s2120405@users.noreply.github.com>
2022-03-29 09:59:12 +0800

    Create index.php

---
+++ LT2/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+		<head>
+		<title>My Home Page</title>
+		</head>
+  <body>
+    <h1>Welcome to My Website! I am Kirby Calampinay</h1>
+    <ul>
+      <li><a href="/LT2/index.html">LEARNING TASK 2</a></li>
+	<li><a href="/LT3/index.html">LEARNING TASK 3</a></li>
+	    <li><a href="/CLE1MT/index.html">Culminating Learning Evidence (Midterm)</a></li>
+    </ul>
+  </body>
+</html>
