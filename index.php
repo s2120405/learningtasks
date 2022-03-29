f6348450921b15f42e674591e669a6bfa1c59f48
s2120405 <92307093+s2120405@users.noreply.github.com>
2022-03-29 09:55:45 +0800

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   <body>
     <h1>Welcome to My Website! I am Kirby Calampinay</h1>
     <ul>
-      <li><a href="/LT2/RecipeHOME.html">LEARNING TASK 2</a></li>
+      <li><a href="/LT2/index.html">LEARNING TASK 2</a></li>
 	<li><a href="/LT3/MyList.html">LEARNING TASK 3</a></li>
 	    <li><a href="/CLE1MT/index.html">Culminating Learning Evidence (Midterm)</a></li>
     </ul>
