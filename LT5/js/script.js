c67cd1aa4a106fe1f63462b0e225062b625b439b
s2120405 <92307093+s2120405@users.noreply.github.com>
2022-04-22 20:07:58 +0800

    Add files via upload

---
+++ LT5/js/script.js
@@ -0,0 +1,38 @@
+/*eslint-env browser*/
+
+function Pokerotate1() {
+  document.getElementById("PokeCapsule1").src = "pics/pokeshake.gif";
+}
+function Pokestop1(){
+  document.getElementById("PokeCapsule1").src = "pics/pokeshake.png";
+}
+function Pokeclick1(){
+  document.getElementById("PokeCapsule1").src = "pics/charmander.gif";
+  var audio = new Audio('pics/charmanderscream.mp3');
+  audio.play();
+}
+
+ 
+function Pokerotate2() {
+  document.getElementById("PokeCapsule2").src = "pics/pokeshake.gif";
+}
+function Pokestop2(){
+  document.getElementById("PokeCapsule2").src = "pics/pokeshake.png";
+}
+function Pokeclick2(){
+  document.getElementById("PokeCapsule2").src = "pics/bulbasaur.gif";
+  var audio = new Audio('pics/bulbascream.mp3');
+  audio.play();
+}
+
+function Pokerotate3() {
+  document.getElementById("PokeCapsule3").src = "pics/pokeshake.gif";
+}
+function Pokestop3(){
+  document.getElementById("PokeCapsule3").src = "pics/pokeshake.png";
+}
+function Pokeclick3(){
+  document.getElementById("PokeCapsule3").src = "pics/squirtle.gif";
+  var audio = new Audio('pics/squirtlescream.mp3');
+  audio.play();
+}
