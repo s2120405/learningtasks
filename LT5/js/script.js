84d5e1c98ad9266ad44a012ced55dbd65f5f6916
s2120405 <92307093+s2120405@users.noreply.github.com>
2022-04-22 18:53:34 +0800

    Add files via upload

---
+++ LT5/js/script.js
@@ -0,0 +1,31 @@
+/*eslint-env browser*/
+
+function Pokerotate1() {
+  document.getElementById("PokeCapsule1").src = "pics/pokeshake.gif";
+}
+function Pokestop1(){
+  document.getElementById("PokeCapsule1").src = "pics/pokeshake.png";
+}
+function Pokeclick1(){
+  document.getElementById("PokeCapsule1").src = "pics/charmander.gif";
+}
+
+function Pokerotate2() {
+  document.getElementById("PokeCapsule2").src = "pics/pokeshake.gif";
+}
+function Pokestop2(){
+  document.getElementById("PokeCapsule2").src = "pics/pokeshake.png";
+}
+function Pokeclick2(){
+  document.getElementById("PokeCapsule2").src = "pics/bulbasaur.gif";
+}
+
+function Pokerotate3() {
+  document.getElementById("PokeCapsule3").src = "pics/pokeshake.gif";
+}
+function Pokestop3(){
+  document.getElementById("PokeCapsule3").src = "pics/pokeshake.png";
+}
+function Pokeclick3(){
+  document.getElementById("PokeCapsule3").src = "pics/squirtle.gif";
+}
