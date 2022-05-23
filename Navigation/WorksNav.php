d93cd02fb6dc238a5e1694d26058fa65c71ca2ea
s2120405 <92307093+s2120405@users.noreply.github.com>
2022-05-23 08:50:55 +0800

    Add files via upload

--- Navigation/WorksNav.php
+++ Navigation/WorksNav.php
@@ -1,20 +1,20 @@
 <div id="box6">
-	<h6 onclick ='Alert()'><a onmouseenter='play()' onclick ='play1()' href="CLE1MT/Homepage.html" target="_blank">CLE1MT</a></h6>
-	<img class="img3" src="misc/OverPop.png" alt="CLE1MT">
+	<h6 onclick ='Alert4()'><a onmouseenter='play()' onclick ='play1()' href="LT2/RecipeHOME.html" target="_blank">Learning Task 2</a></h6>
+	<img class="img3" src="misc/Recipes.png" alt="LT2">
 	</div>
 	<div id="box6">
-	<h6 onclick ='Alert1()'><a onmouseenter='play()' onclick ='play1()' href="LT5/index.html" target="_blank">Learning Task 5</a></h6>
-	<img class="img3" src="misc/Java.png" alt="LT5">
+	<h6 onclick ='Alert3()'><a onmouseenter='play()' onclick ='play1()' href="LT3/MyList.html" target="_blank">Learning Task 3</a></h6>
+	<img class="img3" src="misc/List.png" alt="LT3">
 	</div>
 	<div id="box6">
 	<h6 onclick ='Alert2()'><a onmouseenter='play()' onclick ='play1()' href="LT4/index.html" target="_blank">Learning Task 4</a></h6>
 	<img class="img3" src="misc/Poem.png" alt="LT4">
 	</div>
 	<div id="box6">
-	<h6 onclick ='Alert3()'><a onmouseenter='play()' onclick ='play1()' href="LT3/index.html" target="_blank">Learning Task 3</a></h6>
-	<img class="img3" src="misc/List.png" alt="LT3">
+	<h6 onclick ='Alert1()'><a onmouseenter='play()' onclick ='play1()' href="LT5/index.html" target="_blank">Learning Task 5</a></h6>
+	<img class="img3" src="misc/Java.png" alt="LT5">
 	</div>
-	<div id="box6">
-	<h6 onclick ='Alert4()'><a onmouseenter='play()' onclick ='play1()' href="LT2/RecipeHOME.html" target="_blank">Learning Task 2</a></h6>
-	<img class="img3" src="misc/Recipes.png" alt="LT2">
+	<div id="box8">
+	<h6 onclick ='Alert()'><a onmouseenter='play()' onclick ='play1()' href="CLE1MT/Homepage.html" target="_blank">CLE1MT</a></h6>
+	<img class="img3" src="misc/OverPop.png" alt="CLE1MT">
 	</div>
